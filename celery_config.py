288f745e79f35157c5e72fed8d54ed31e585f350
Dimitrios Misdanitis <d.misdanitis@gmail.com>
2018-04-22 06:01:27 +0300

    add configuration for rabbitmq server url

--- celery_config.py
+++ celery_config.py
@@ -2,8 +2,9 @@ import json
 
 conf = json.load(open('conf.json'))
 
-BROKER_URL = 'amqp://%s:%s@localhost:5672/%s' % (
+BROKER_URL = 'amqp://%s:%s@%s/%s' % (
     conf['rabbitmq_user'],
     conf['rabbitmq_password'],
+    conf['rabbitmq_host_url'],
     conf['rabbitmq_vhost'],
 )
