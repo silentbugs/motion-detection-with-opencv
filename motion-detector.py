980d9fc42aef1d30eae42cd1d15badd2563332af
Dimitrios Misdanitis <d.misdanitis@gmail.com>
2018-06-09 22:07:16 +0300

    add option to disable celery

--- motion-detector.py
+++ motion-detector.py
@@ -112,7 +112,9 @@ class MotionDetector:
                     # check to see if the number of frames with consistent
                     # motion is high enough
                     if motion_counter >= self.conf['main']['min_motion_frames']:
-                        exec_notify.delay()
+                        if self.conf['main']['use_celery']:
+                            exec_notify.delay()
+
                         path = self.conf['extra']['output_directory']
                         _file = timestamp.strftime('%Y_%m_%dT%H_%M_%S' + '.jpg')
                         cv2.imwrite(path + _file, frame)
